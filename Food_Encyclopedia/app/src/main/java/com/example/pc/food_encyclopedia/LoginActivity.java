50a752c52f744f7a2b5543cf22160a9fcbbc1489
ashwin.alias@gmail.com <ashwin.alias@gmail.com>
2018-08-24 18:26:18 +0530

    Integrated fb login

--- Food_Encyclopedia/app/src/main/java/com/example/pc/food_encyclopedia/LoginActivity.java
+++ Food_Encyclopedia/app/src/main/java/com/example/pc/food_encyclopedia/LoginActivity.java
@@ -1,25 +1,67 @@
 package com.example.pc.food_encyclopedia;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
+    LoginButton loginButton;
+    CallbackManager mFbCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_layout);
         initScreens();
-
     }
 
     private void initScreens()
     {
+
+        //setting font style
         getSupportActionBar().hide();
         TextView textView=findViewById(R.id.welcome_foodes);
         Typeface type = Typeface.createFromAsset(getAssets(),"ZapChance.ttf");
         textView.setTypeface(type);
+
+         //setting up Fb Login
+        loginButton = (LoginButton) findViewById(R.id.login_button_fb);
+        loginButton.setReadPermissions(Arrays.asList("email"));
+        mFbCallbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(mFbCallbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        Toast.makeText(LoginActivity.this, "Fb login success", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Toast.makeText(LoginActivity.this, "Fb login  cancel", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        Toast.makeText(LoginActivity.this, "Fb login  Failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mFbCallbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
