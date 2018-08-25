bd18247314d7812eec8ba11f720ca1425b76dc70
ashwin.alias@gmail.com <ashwin.alias@gmail.com>
2018-08-25 10:50:39 +0530

    Adding google login

--- Food_Encyclopedia/app/src/main/java/com/example/pc/food_encyclopedia/LoginActivity.java
+++ Food_Encyclopedia/app/src/main/java/com/example/pc/food_encyclopedia/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,12 +14,20 @@ import com.facebook.FacebookException;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 import java.util.Arrays;
 
-public class LoginActivity extends AppCompatActivity {
-    LoginButton loginButton;
-    CallbackManager mFbCallbackManager;
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private LoginButton loginButton;
+    private CallbackManager mFbCallbackManager;
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +36,15 @@ public class LoginActivity extends AppCompatActivity {
         initScreens();
     }
 
-    private void initScreens()
-    {
+    private void initScreens() {
 
         //setting font style
         getSupportActionBar().hide();
-        TextView textView=findViewById(R.id.welcome_foodes);
-        Typeface type = Typeface.createFromAsset(getAssets(),"ZapChance.ttf");
+        TextView textView = findViewById(R.id.welcome_foodes);
+        Typeface type = Typeface.createFromAsset(getAssets(), "ZapChance.ttf");
         textView.setTypeface(type);
 
-         //setting up Fb Login
+        //setting up Fb Login
         loginButton = (LoginButton) findViewById(R.id.login_button_fb);
         loginButton.setReadPermissions(Arrays.asList("email"));
         mFbCallbackManager = CallbackManager.Factory.create();
@@ -57,11 +65,51 @@ public class LoginActivity extends AppCompatActivity {
                         Toast.makeText(LoginActivity.this, "Fb login  Failed", Toast.LENGTH_SHORT).show();
                     }
                 });
+
+
+        //Setting up Google Login
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        SignInButton signInButton = findViewById(R.id.sign_in_button_glge);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setOnClickListener(this);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 25) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
         mFbCallbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button_glge:
+                signInGoogle();
+                break;
+        }
+    }
+
+
+   //Method used to sign in into google
+    private void signInGoogle() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, 25);
+    }
+
+   //This method recives google sign in info (if success)
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            Toast.makeText(this, "Google sign in success " + account.getEmail(), Toast.LENGTH_SHORT).show();
+        } catch (ApiException e) {
+            Toast.makeText(this, "Google sign in falure ", Toast.LENGTH_SHORT).show();
+        }
     }
 }
